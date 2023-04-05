a9e6dd0432ed6b1e53923fd30e9392e424f546a1
vladislav shakhov <shahov199@gmail.com>
2023-04-05 20:06:36 +0300

    homework

--- Homework.java
+++ Homework.java
@@ -10,23 +10,35 @@ public class Homework {
         strings.add("40");
         strings.add("-5");
         strings.add("my_string");
-        //removeIntegers(strings);
-        //System.out.println(strings);
-        removeEvenNumber(strings);
-        System.out.println(strings); // [string, my_string]
+        removeIntegers(strings);
+        System.out.println(strings); 
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(5);
+        numbers.add(8);
+        numbers.add(9);
+        removeEvenNumber(numbers);
+        System.out.println(numbers); 
     }
     
-    static void removeEvenNumber(List<String> strings) {
+    static void removeEvenNumber(List<Integer> numbers) {
         // TODO: 31.03.2023 Удалить все четные элементы из списка
-        for (int i = 1; i < strings.size(); i++) {
-            if (i % 2 == 0) {
-                strings.remove(i);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % 2 == 0) {
+                numbers.remove(i);
             }
-            
         }
     }
     
     static void removeIntegers(List<String> strings) {
         // TODO: 31.03.2023 Удалить строки, которые являются целыми числами
+        for (int i = 0; i < strings.size(); i++) {
+            try {
+                Integer.parseInt(strings.get(i));
+                strings.remove(i);
+            } catch (NumberFormatException e) {
+            }
+        }
     }
 }
